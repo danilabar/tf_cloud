f4721445d5b64d2d9bd4e613034426ad24a837b0
danilabar <92711474+danilabar@users.noreply.github.com>
2022-03-24 08:57:55 +0300

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,20 @@
+variable "yc_token" {
+   default = ""
+}
+
+variable "yc_cloud_id" {
+  default = ""
+}
+
+variable "yc_folder_id" {
+  default = ""
+}
+
+variable "yc_region" {
+  default = "ru-central1-a"
+}
+
+variable "yc_image_id" {
+  default = "fd81hgrcv6lsnkremf32"
+}
+
